018ad70adbdd95ed204bdd4ee485bbb807646aa0
Maurice Shalam <mss619@nyu.edu>
2019-04-01 22:54:20 -0400

    fixed favicon

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,7 +2,6 @@ module.exports = {
   siteMetadata: {
     title: 'Maurice Shalam',
   },
-  pathPrefix: '/personalwebsite',
   plugins: [
     'gatsby-plugin-react-helmet',
     'gatsby-plugin-netlify',
